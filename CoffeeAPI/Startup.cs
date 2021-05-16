1bf98aa08e2d634862eb92dd879823de7ecb6017
krasnia2 <krasnia2@gmail.com>
2021-05-16 16:00:42 -0400

    Add Envoy Gateway

--- CoffeeAPI/Startup.cs
+++ CoffeeAPI/Startup.cs
@@ -36,8 +36,6 @@ namespace CoffeeAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
